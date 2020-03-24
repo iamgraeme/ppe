44f3811e0aa202e341d691a5d6736281270d7fa1
Graeme <hello@iamgrae.me>
2020-03-24 07:49:34 +0000

    Fix headers issue

--- store/index.js
+++ store/index.js
@@ -40,7 +40,9 @@ export const actions = {
     commit("clearCart");
   },
   nuxtServerInit({ dispatch }, { req }) {
-    console.log(req.headers.cookie);
+    if (process.server && process.static) return;
+    if (!req.headers.cookie) return;
+
     const cookies = Cookie.parse(req.headers.cookie || "");
     const token = cookies["access_token"] || "";
 
